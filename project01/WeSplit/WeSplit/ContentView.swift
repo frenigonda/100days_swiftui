02427e1ba8901c5b9c2725dd305ca0d807e76580
Daria Frolova <frenigonda@gmail.com>
2024-03-02 22:35:36 +0000

    01: warm up before actual coding

--- project01/WeSplit/WeSplit/ContentView.swift
+++ project01/WeSplit/WeSplit/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
+    @State private var name = ""
+    
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section("Hey") {
+                    Text("Hello, world!")
+                    Text("Hello, world!")
+                }
+                
+                Section("Tap count") {
+                    Button("Tap count: \(tapCount)") {
+                        self.tapCount += 1
+                    }
+                }
+                
+                Section("Name") {
+                    TextField("What's your name?", text: $name)
+                    Text("Your name is \(name)")
+                }
+                
+                Section("Views in a loop") {
+                    ForEach(1..<6) { number in
+                        Text("Row \(number)")
+                    }
+                }
+                
+                Section("Hogwarts students") {
+                    Picker("Select your student", selection: $selectedStudent) {
+                        ForEach(students, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("SwiftUI")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
